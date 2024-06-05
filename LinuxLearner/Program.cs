c3b4912fae910ad21bb39136fec8c28aba3b38de
kanpov <karpovanton729@gmail.com>
2024-06-05 15:33:11 +0300

    Set up authentication and authorization using Keycloak

--- LinuxLearner/Program.cs
+++ LinuxLearner/Program.cs
@@ -1,7 +1,13 @@
+using System.Security.Claims;
+using Keycloak.AuthServices.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddKeycloakWebApiAuthentication(builder.Configuration);
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -10,5 +16,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/", (ClaimsPrincipal principal) =>
+{
+    return "";
+}).RequireAuthorization();
 
 app.Run();
