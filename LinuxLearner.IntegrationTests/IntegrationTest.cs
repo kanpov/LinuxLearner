41a95fad71fe14efb47e3cc5d4be86ec35682e55
kanpov <karpovanton729@gmail.com>
2024-06-12 20:33:59 +0300

    Uncomment and refactor tests, 18/27 currently are passing

--- LinuxLearner.IntegrationTests/IntegrationTest.cs
+++ LinuxLearner.IntegrationTests/IntegrationTest.cs
@@ -1,92 +1,120 @@
-// using System.Security.Claims;
-// using Keycloak.AuthServices.Sdk.Admin;
-// using Keycloak.AuthServices.Sdk.Admin.Models;
-// using LinuxLearner.Database;
-// using LinuxLearner.Domain;
-// using LinuxLearner.Features.Users;
-// using Microsoft.AspNetCore.Http;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.Extensions.DependencyInjection;
-//
-// namespace LinuxLearner.IntegrationTests;
-//
-// public class IntegrationTest : IClassFixture<IntegrationTestFactory>
-// {
-//     protected readonly IServiceProvider Services;
-//     protected AppDbContext DbContext => Services.GetRequiredService<AppDbContext>();
-//     protected IKeycloakUserClient KeycloakUserClient => Services.GetRequiredService<IKeycloakUserClient>();
-//
-//     private static bool _migrationComplete;
-//     
-//     protected IntegrationTest(IntegrationTestFactory factory)
-//     {
-//         Services = factory.Services.CreateScope().ServiceProvider;
-//
-//         if (_migrationComplete) return;
-//         DbContext.Database.Migrate();
-//         _migrationComplete = true;
-//     }
-//
-//     protected async Task<string> CreateKeycloakUserAsync(User baseUser)
-//     {
-//         return await CreateKeycloakUserAsync(baseUser.Name, baseUser.UserType);
-//     }
-//
-//     protected async Task<string> CreateKeycloakUserAsync(string username, UserType userType = UserType.Student)
-//     {
-//         await KeycloakUserClient.CreateUserAsync("master", new UserRepresentation
-//         {
-//             Username = username,
-//             Enabled = true
-//         });
-//
-//         var userService = Services.GetRequiredService<UserService>();
-//         
-//         var keycloakUserId = await userService.GetKeycloakUserId(username);
-//         
-//         var studentsId = await userService.GetKeycloakGroupId(UserType.Student);
-//         var teachersId = await userService.GetKeycloakGroupId(UserType.Teacher);
-//         var adminsId = await userService.GetKeycloakGroupId(UserType.Admin);
-//
-//         await KeycloakUserClient.JoinGroupAsync("master", keycloakUserId, studentsId);
-//
-//         if (userType != UserType.Student)
-//         {
-//             await KeycloakUserClient.JoinGroupAsync("master", keycloakUserId, teachersId);
-//         }
-//
-//         if (userType == UserType.Admin)
-//         {
-//             await KeycloakUserClient.JoinGroupAsync("master", keycloakUserId, adminsId);
-//         }
-//
-//         return keycloakUserId;
-//     }
-//
-//     protected static DefaultHttpContext MakeContext(UserType userType)
-//     {
-//         return userType switch
-//         {
-//             UserType.Student => MakeContext("student"),
-//             UserType.Teacher => MakeContext("teacher"),
-//             UserType.Admin => MakeContext("admin"),
-//             _ => throw new ArgumentOutOfRangeException(nameof(userType), userType, null)
-//         };
-//     }
-//     
-//     private static DefaultHttpContext MakeContext(string role)
-//     {
-//         var httpContext = new DefaultHttpContext();
-//         
-//         var claimsPrincipal = new ClaimsPrincipal();
-//         claimsPrincipal.AddIdentity(new ClaimsIdentity(
-//         [
-//             new Claim("preferred_username", role + Random.Shared.Next(100000, 1000000)),
-//             new Claim("resource_access", role)
-//         ], "Bearer", "preferred_username", "resource_access"));
-//
-//         httpContext.User = claimsPrincipal;
-//
-//         return httpContext;
-//     }
-// }
+using System.Security.Claims;
+using Keycloak.AuthServices.Sdk.Admin;
+using Keycloak.AuthServices.Sdk.Admin.Models;
+using Keycloak.AuthServices.Sdk.Admin.Requests.Users;
+using LinuxLearner.Database;
+using LinuxLearner.Domain;
+using LinuxLearner.Features.Users;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LinuxLearner.IntegrationTests;
+
+public class IntegrationTest : IClassFixture<IntegrationTestFactory>
+{
+    protected readonly IServiceProvider Services;
+    protected AppDbContext DbContext => Services.GetRequiredService<AppDbContext>();
+    protected IKeycloakUserClient KeycloakUserClient => Services.GetRequiredService<IKeycloakUserClient>();
+
+    private static bool _migrationComplete;
+    
+    protected IntegrationTest(IntegrationTestFactory factory)
+    {
+        Services = factory.Services.CreateScope().ServiceProvider;
+
+        if (_migrationComplete) return;
+        DbContext.Database.Migrate();
+        _migrationComplete = true;
+    }
+
+    protected async Task<Guid> CreateKeycloakUserAsync(User baseUser)
+    {
+        return await CreateKeycloakUserAsync(baseUser.UserType);
+    }
+
+    protected async Task<Guid> CreateKeycloakUserAsync(UserType userType = UserType.Student)
+    {
+        var username = "test-user-" + Guid.NewGuid();
+        await KeycloakUserClient.CreateUserAsync("master", new UserRepresentation
+        {
+            Username = username,
+            Enabled = true
+        });
+
+        var userService = Services.GetRequiredService<UserService>();
+
+        var matchingUsers = await KeycloakUserClient.GetUsersAsync("master", new GetUsersRequestParameters
+        {
+            Username = username,
+            Exact = true,
+            Max = 1,
+            BriefRepresentation = true
+        });
+        var keycloakUserId = matchingUsers.First().Id!;
+        
+        var studentsId = await userService.GetKeycloakGroupId(UserType.Student);
+        var teachersId = await userService.GetKeycloakGroupId(UserType.Teacher);
+        var adminsId = await userService.GetKeycloakGroupId(UserType.Admin);
+
+        await KeycloakUserClient.JoinGroupAsync("master", keycloakUserId, studentsId);
+
+        if (userType != UserType.Student)
+        {
+            await KeycloakUserClient.JoinGroupAsync("master", keycloakUserId, teachersId);
+        }
+
+        if (userType == UserType.Admin)
+        {
+            await KeycloakUserClient.JoinGroupAsync("master", keycloakUserId, adminsId);
+        }
+
+        return Guid.Parse(keycloakUserId);
+    }
+
+    protected async Task<DefaultHttpContext> MakeContextAndUserAsync(UserType userType)
+    {
+        var keycloakUserId = await CreateKeycloakUserAsync(userType);
+        var httpContext = MakeContext(userType, keycloakUserId);
+        return httpContext;
+    }
+
+    protected static DefaultHttpContext MakeContext(UserType userType, Guid? userId = null)
+    {
+        return userType switch
+        {
+            UserType.Student => MakeContext("student", userId),
+            UserType.Teacher => MakeContext("teacher", userId),
+            UserType.Admin => MakeContext("admin", userId),
+            _ => throw new ArgumentOutOfRangeException(nameof(userType), userType, null)
+        };
+    }
+
+    protected static Guid GetUserIdFromContext(HttpContext httpContext)
+    {
+        var stringValue = httpContext.User.Claims.First(c => c.Type.EndsWith("nameidentifier")).Value;
+        return Guid.Parse(stringValue);
+    }
+    
+    private static DefaultHttpContext MakeContext(string role, Guid? userId = null)
+    {
+        var httpContext = new DefaultHttpContext();
+        
+        var claimsPrincipal = new ClaimsPrincipal();
+        var identity = new ClaimsIdentity(
+        [
+            new Claim("preferred_username", role + Random.Shared.Next(100000, 1000000)),
+            new Claim("resource_access", role),
+        ], "Bearer", "preferred_username", "resource_access");
+        if (userId is not null)
+        {
+            identity.AddClaim(new Claim("nameidentifier", userId.ToString()!));
+        }
+        
+        claimsPrincipal.AddIdentity(identity);
+
+        httpContext.User = claimsPrincipal;
+
+        return httpContext;
+    }
+}
