c0e42a389434082d36879a954f98a9c93a0f4cdf
kanpov <karpovanton729@gmail.com>
2024-06-13 09:42:03 +0300

    Improve route parameter naming

--- LinuxLearner/Features/Courses/CourseEndpoints.cs
+++ LinuxLearner/Features/Courses/CourseEndpoints.cs
@@ -9,14 +9,14 @@ public static class CourseEndpoints
 {
     public static void Map(RouteGroupBuilder studentApi, RouteGroupBuilder teacherApi, RouteGroupBuilder adminApi)
     {
-        studentApi.MapGet("/courses/{id:guid}", GetCourse).WithName(nameof(GetCourse));
+        studentApi.MapGet("/courses/{courseId:guid}", GetCourse).WithName(nameof(GetCourse));
         
         teacherApi.MapPost("/courses", CreateCourse);
-        teacherApi.MapPatch("/courses/{id:guid}", PatchCourse);
-        teacherApi.MapDelete("/courses/{id:guid}", DeleteCourse);
+        teacherApi.MapPatch("/courses/{courseId:guid}", PatchCourse);
+        teacherApi.MapDelete("/courses/{courseId:guid}", DeleteCourse);
 
-        adminApi.MapPatch("/courses/{id:guid}/force", ForcePatchCourse);
-        adminApi.MapDelete("/courses/{id:guid}/force", ForceDeleteCourse);
+        adminApi.MapPatch("/courses/{courseId:guid}/force", ForcePatchCourse);
+        adminApi.MapDelete("/courses/{courseId:guid}/force", ForceDeleteCourse);
     }
 
     private static async Task<Results<ValidationProblem, CreatedAtRoute<CourseDto>>> CreateCourse(
@@ -31,45 +31,45 @@ public static class CourseEndpoints
     }
 
     private static async Task<Results<ValidationProblem, NotFound, NoContent>> PatchCourse(
-        CourseService courseService, Guid id, CoursePatchDto coursePatchDto,
+        CourseService courseService, Guid courseId, CoursePatchDto coursePatchDto,
         IValidator<CoursePatchDto> validator, HttpContext httpContext)
     {
         var validationResult = await validator.ValidateAsync(coursePatchDto);
         if (!validationResult.IsValid) return validationResult.ToProblem(httpContext);
 
-        var success = await courseService.PatchCourseAsync(httpContext, id, coursePatchDto);
+        var success = await courseService.PatchCourseAsync(httpContext, courseId, coursePatchDto);
         return success ? TypedResults.NoContent() : TypedResults.NotFound();
     }
 
-    private static async Task<Results<NotFound, NoContent>> DeleteCourse(CourseService courseService, Guid id,
+    private static async Task<Results<NotFound, NoContent>> DeleteCourse(CourseService courseService, Guid courseId,
         HttpContext httpContext)
     {
-        var success = await courseService.DeleteCourseAsync(httpContext, id);
+        var success = await courseService.DeleteCourseAsync(httpContext, courseId);
         return success ? TypedResults.NoContent() : TypedResults.NotFound();
     }
 
     private static async Task<Results<NotFound, Ok<CourseDto>>> GetCourse(
-        CourseService courseService, Guid id)
+        CourseService courseService, Guid courseId)
     {
-        var courseDto = await courseService.GetCourseAsync(id);
+        var courseDto = await courseService.GetCourseAsync(courseId);
         if (courseDto is null) return TypedResults.NotFound();
         return TypedResults.Ok(courseDto);
     }
 
-    private static async Task<Results<NotFound, NoContent>> ForceDeleteCourse(CourseService courseService, Guid id)
+    private static async Task<Results<NotFound, NoContent>> ForceDeleteCourse(CourseService courseService, Guid courseId)
     {
-        var success = await courseService.ForceDeleteCourseAsync(id);
+        var success = await courseService.ForceDeleteCourseAsync(courseId);
         return success ? TypedResults.NoContent() : TypedResults.NotFound();
     }
 
     private static async Task<Results<ValidationProblem, NotFound, NoContent>> ForcePatchCourse(
-        CourseService courseService, Guid id, CoursePatchDto coursePatchDto,
+        CourseService courseService, Guid courseId, CoursePatchDto coursePatchDto,
         IValidator<CoursePatchDto> validator, HttpContext httpContext)
     {
         var validationResult = await validator.ValidateAsync(coursePatchDto);
         if (!validationResult.IsValid) return validationResult.ToProblem(httpContext);
 
-        var success = await courseService.ForcePatchCourseAsync(id, coursePatchDto);
+        var success = await courseService.ForcePatchCourseAsync(courseId, coursePatchDto);
         return success ? TypedResults.NoContent() : TypedResults.NotFound();
     }
 }
